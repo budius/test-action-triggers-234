ad0a7f1b03338bb6630c0cf6570e80366163d0e7
Ronaldo Pace <ronaldopace@gmail.com>
2021-09-30 15:36:03 +0200

    Update check_tag.sh

--- check_tag.sh
+++ check_tag.sh
@@ -1,8 +1,12 @@
+echo "let's try"
 if [[ $1 =~ ^refs/tags/([0-9]+).([0-9]+).([0-9]+)$ ]]; then
+    echo "type 123 and abc"
     echo ::set-output name=TAG_BUILD_TYPES::TYPE_123,TYPE_ABC
 elif [[ $1 =~ ^refs/tags/([0-9]+).([0-9]+).(([0-9]+)|X)-S([0-9]+)$ ]]; then
+    echo "type xyz"
     echo ::set-output name=TAG_BUILD_TYPES::TYPE_XYZ
 else
+    ehco "not allowed"
     echo ::set-output name=TAG_BUILD_TYPES::NO
     exit 1
 fi
