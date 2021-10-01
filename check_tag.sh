83a6dd08bfb2acad99d4683dbba09c3cdaf32985
Ronaldo Pace <ronaldopace@gmail.com>
2021-10-01 14:21:01 +0200

    Update check_tag.sh

--- check_tag.sh
+++ check_tag.sh
@@ -1,8 +1,9 @@
 GH_TAG=$1
 MY_BUILD_TYPE=$2
 echo "GH_TAG = $GH_TAG and MY_BUILD_TYPE = $MY_BUILD_TYPE"
+# Check for release version
 if [[ $GH_TAG =~ ^refs/tags/([0-9]+).([0-9]+).([0-9]+)$ ]]; then
-    echo "type 123 or abc"
+    echo "Type Release"
     if [[ "TYPE_123" == $MY_BUILD_TYPE ]] || [[ "TYPE_ABC" == $MY_BUILD_TYPE ]]; then
         echo "continue = true"
         echo "::set-output name=CONTINUE::true"
@@ -10,8 +11,19 @@ if [[ $GH_TAG =~ ^refs/tags/([0-9]+).([0-9]+).([0-9]+)$ ]]; then
         echo "continue = false"
         echo "::set-output name=CONTINUE::false"
     fi
+# Check for RC version
+elif [[ $GH_TAG =~ ^refs/tags/([0-9]+).([0-9]+).([0-9]+)(-RC\.[0-9]+)$ ]]; then
+    echo "Type Release Candidate"
+    if [[ "TYPE_ABC" == $MY_BUILD_TYPE ]]; then
+        echo "continue = true"
+        echo "::set-output name=CONTINUE::true"
+    else
+        echo "continue = false"
+        echo "::set-output name=CONTINUE::false"
+    fi
+# Check for sprint version
 elif [[ $GH_TAG =~ ^refs/tags/([0-9]+).([0-9]+).(([0-9]+)|X)-S([0-9]+)$ ]]; then
-    echo "type xyz"
+    echo "Type Sprint"
     if [[ "TYPE_XYZ" == $MY_BUILD_TYPE ]]; then
         echo "continue = true"
         echo "::set-output name=CONTINUE::true"
