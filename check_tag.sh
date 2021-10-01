d2c510c19cc124059b8e831268f6491954237c48
Ronaldo Pace <ronaldopace@gmail.com>
2021-10-01 13:49:12 +0200

    Update check_tag.sh

--- check_tag.sh
+++ check_tag.sh
@@ -2,8 +2,8 @@ GH_TAG=$1
 MY_BUILD_TYPE=$2
 echo "GH_TAG = $GH_TAG and MY_BUILD_TYPE = $MY_BUILD_TYPE"
 if [[ $GH_TAG =~ ^refs/tags/([0-9]+).([0-9]+).([0-9]+)$ ]]; then
+    echo "type 123 or abc"
     if [[ "TYPE_123" == $MY_BUILD_TYPE ]] || [[ "TYPE_ABC" == $MY_BUILD_TYPE ]]; then
-        echo "type 123 or abc"
         echo "continue = true"
         echo "::set-output name=CONTINUE::true"
     else
