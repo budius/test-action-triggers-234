8523bd8ca876591f43bb41293e6c0be2e0fbb295
Ronaldo Pace <ronaldopace@gmail.com>
2021-10-01 13:46:23 +0200

    Update check_tag.sh

--- check_tag.sh
+++ check_tag.sh
@@ -2,17 +2,21 @@ GH_TAG=$1
 MY_BUILD_TYPE=$2
 echo "GH_TAG = $GH_TAG and MY_BUILD_TYPE = $MY_BUILD_TYPE"
 if [[ $GH_TAG =~ ^refs/tags/([0-9]+).([0-9]+).([0-9]+)$ ]]; then
-    echo "type 123 or abc"
-    if [[ "TYPE_123" == "$MY_BUILD_TYPE" ]] || [[ "TYPE_ABC" == "$MY_BUILD_TYPE" ]]; then
+    if [[ "TYPE_123" == $MY_BUILD_TYPE ]] || [[ "TYPE_ABC" == $MY_BUILD_TYPE ]]; then
+        echo "type 123 or abc"
+        echo "continue = true"
         echo "::set-output name=CONTINUE::true"
     else
+        echo "continue = false"
         echo "::set-output name=CONTINUE::false"
     fi
-elif [[ $1 =~ ^refs/tags/([0-9]+).([0-9]+).(([0-9]+)|X)-S([0-9]+)$ ]]; then
+elif [[ $GH_TAG =~ ^refs/tags/([0-9]+).([0-9]+).(([0-9]+)|X)-S([0-9]+)$ ]]; then
     echo "type xyz"
-    if [[ "TYPE_XYZ" == "$MY_BUILD_TYPE" ]]; then
+    if [[ "TYPE_XYZ" == $MY_BUILD_TYPE ]]; then
+        echo "continue = true"
         echo "::set-output name=CONTINUE::true"
     else
+        echo "continue = false"
         echo "::set-output name=CONTINUE::false"
     fi
 else
