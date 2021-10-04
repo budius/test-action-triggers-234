fd0b0047dbebbf65e2a6622226fd621031f52100
Ronaldo Pace <ronaldopace@gmail.com>
2021-10-04 10:15:59 +0200

    Update check_tag.sh

--- check_tag.sh
+++ check_tag.sh
@@ -2,8 +2,8 @@ GH_TAG=$1
 MY_BUILD_TYPE=$2
 echo "GH_TAG = $GH_TAG and MY_BUILD_TYPE = $MY_BUILD_TYPE"
 
-TO_REMOVE="refs/tags/"
-PLAIN_TAG="${GH_TAG/TO_REMOVE/""}"
+
+PLAIN_TAG="${GH_TAG//refs\/tags\/}"
 
 echo $PLAIN_TAG
 
